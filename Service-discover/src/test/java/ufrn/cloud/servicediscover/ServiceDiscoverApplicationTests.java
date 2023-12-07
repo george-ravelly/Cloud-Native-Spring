c4a493c38079f24e7ca8fd7faabd927f464e6d49
george-ravelly <george.ravelly1@gmail.com>
2023-12-07 20:30:03 -0300

    Add: eureka server e actuator

---
+++ Service-discover/src/test/java/ufrn/cloud/servicediscover/ServiceDiscoverApplicationTests.java
@@ -0,0 +1,13 @@
+package ufrn.cloud.servicediscover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
