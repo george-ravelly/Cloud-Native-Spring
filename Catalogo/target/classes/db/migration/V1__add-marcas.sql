20bb8c4ee71e6c1f759f55b7f429f23649bca3b8
george-ravelly <george.ravelly1@gmail.com>
2023-12-13 21:32:20 -0300

    add: migrations script de migrations

---
+++ Catalogo/target/classes/db/migration/V1__add-marcas.sql
@@ -0,0 +1,11 @@
+insert into marca (cod, nome) values
+    (1, 'Adidas'),
+    (2, 'Brastemp'),
+    (3, 'Samsung'),
+    (4, 'Gucci'),
+    (5, 'Lenovo'),
+    (6, 'Puma'),
+    (7, 'Xbox'),
+    (8, 'Sony'),
+    (9, 'Nintendo');
+
