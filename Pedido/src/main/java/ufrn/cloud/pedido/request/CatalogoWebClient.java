f05c7eaffce30346faecd8edf0ed7c8abb3ff8d5
george-ravelly <george.ravelly1@gmail.com>
2023-12-14 19:12:33 -0300

    add: openfeign

--- Pedido/src/main/java/ufrn/cloud/pedido/request/CatalogoWebClient.java
+++ Pedido/src/main/java/ufrn/cloud/pedido/request/CatalogoWebClient.java
@@ -1,4 +1,19 @@
 package ufrn.cloud.pedido.request;
 
-public class CatalogoWebClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
+
+@FeignClient("catalogo")
+public interface CatalogoWebClient {
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/catalogo/estoque/produto/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    Map<String, Object> getData(@PathVariable("id") Long codigoProduto);
 }
--- Pedido/src/main/java/ufrn/cloud/pedido/venda/VendaService.java
+++ Pedido/src/main/java/ufrn/cloud/pedido/venda/VendaService.java
@@ -1,19 +1,29 @@
 package ufrn.cloud.pedido.venda;
 
 import org.springframework.stereotype.Service;
+import ufrn.cloud.pedido.request.CatalogoWebClient;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class VendaService {
     private final VendaRepository repository;
+    private final CatalogoWebClient webClient;
 
-    public VendaService(VendaRepository repository) {
+    public VendaService(VendaRepository repository, CatalogoWebClient webClient) {
         this.repository = repository;
+        this.webClient = webClient;
     }
 
     public Venda save(Venda venda) {
+        venda.getItemVendas().forEach(itemVenda -> {
+            Map<String, Object> produtoEstoque = webClient.getData(itemVenda.getCodigoProduto());
+            if (Integer.getInteger((String) produtoEstoque.get("quantidade")) < itemVenda.getQuantidade()) {
+                throw new RuntimeException("Produto fora de estoque!");
+            }
+        });
         venda.setDataCriacao(LocalDateTime.now());
         return repository.save(venda);
     }
